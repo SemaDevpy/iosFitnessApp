851cd13972f035478fe00fa6911bb5fdf81cfd25
SemaDevpy <syimykdev@gmail.com>
2020-12-04 14:55:03 +0600

    finish approximately functionality of StartPushUpVC

--- FitnessApp/Controller/Second Controllers/StartPushUpViewController.swift
+++ FitnessApp/Controller/Second Controllers/StartPushUpViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class StartPushUpViewController: UIViewController {
     
-    var timer : Timer!
+    var countDown = 5
+    var timer = Timer()
 
     var reps = [Int]()
     
@@ -22,6 +23,7 @@ class StartPushUpViewController: UIViewController {
     @IBOutlet weak var repsLabel: UILabel!
     @IBOutlet weak var currentLabel: UILabel!
     @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var statusLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,15 +40,20 @@ class StartPushUpViewController: UIViewController {
     
 
     @IBAction func btnTapped(_ sender: UIButton) {
-        if numberOfReps != 0{
-            btn.setTitle("\(numberOfReps - 1)", for: .normal)
+        statusLbl.isHidden = true
+        if numberOfReps >= 2{
             numberOfReps = numberOfReps - 1
+            btn.setTitle("\(numberOfReps)", for: .normal)
         }else{
             if numTracker < 4{
+                timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countDownMethod), userInfo: nil, repeats: true)
                 numTracker += 1
                 numberOfReps = reps[numTracker]
                 currentLabel.text = "Current: \(numberOfReps)"
-                btn.setTitle("\(numberOfReps)", for: .normal)
+            }else{
+                statusLbl.isHidden = false
+                btn.setTitle("!", for: .normal)
+                 statusLbl.text = "Workout is complete"
             }
         }
     }
@@ -54,14 +61,41 @@ class StartPushUpViewController: UIViewController {
     
     
     @IBAction func doneBtnTapped(_ sender: UIButton) {
-        numberOfReps = 0
-        btn.setTitle("\(numberOfReps)", for: .normal)
+        statusLbl.isHidden = true
+        if numTracker < 4{
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countDownMethod), userInfo: nil, repeats: true)
+            numTracker += 1
+            numberOfReps = reps[numTracker]
+            currentLabel.text = "Current: \(numberOfReps)"
+        }else{
+            statusLbl.isHidden = false
+            btn.setTitle("!", for: .normal)
+            statusLbl.text = "Workout is complete"
+        }
     }
     
     
     
     //MARK: - Supporting Functionality
-
+    @objc func countDownMethod(){
+        statusLbl.isHidden = false
+        statusLbl.text = "Rest"
+        if countDown != 0{
+            countDown -= 1
+            btn.setTitle("\(countDown)", for: .normal)
+            btn.isEnabled = false
+            doneBtn.isEnabled = false
+        }else{
+            timer.invalidate()
+            statusLbl.isHidden = true
+            btn.setTitle("\(numberOfReps)", for: .normal)
+            countDown = 5
+            btn.isEnabled = true
+            doneBtn.isEnabled = true
+        }
+        
+        
+    }
     
     
     
